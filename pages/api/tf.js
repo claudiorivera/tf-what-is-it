178e8d59cf429f9b0f2209ed71ff7af53cf8eba4
Claudio Rivera <me@claudiorivera.com>
2020-10-31 17:54:46 -0500

    Roll back

--- pages/api/tf.js
+++ pages/api/tf.js
@@ -1,10 +1,8 @@
-const loadTf = require("tensorflow-lambda");
 import * as tfnode from "@tensorflow/tfjs-node";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 
 const handler = async (req, res) => {
   try {
-    await loadTf();
     const base64 = req.body.image.split(",")[1];
     const model = await mobilenet.load();
     const image = tfnode.node.decodeImage(new Buffer.from(base64, "base64"));
